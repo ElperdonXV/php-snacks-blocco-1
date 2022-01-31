efb9f37d09d01d7eca1f89e1fb725cf5fb17aef1
ElperdonXV <davidemartinografico@gmail.com>
2022-01-31 15:53:01 +0100

    second done

---
+++ index2.php
@@ -0,0 +1,49 @@
+<?php
+    $results = [
+        '01-01-2007' =>[
+            [
+                'titolo' => 'Ciao amici',
+                'autore' => 'Carmelo Carmelis',
+                'testo' => 'Sono veramente euforico',
+            ],
+            [
+                'titolo' => 'Ciao amici belli x2',
+                'autore' => 'Gianni Giannetto',
+                'testo' => 'Sono veramente euforico',
+            ],            
+        ],
+        '01-03-2007' =>[
+            [
+                'titolo' => 'Nuovo array',
+                'autore' => 'Claudio De Claudis',
+                'testo' => 'I love php',
+            ],
+            [
+                'titolo' => 'Nuovo o usato',
+                'autore' => 'Divertimento Assicurato',
+                'testo' => 'Vieni in concessionaria',
+            ]
+        ]
+    ];
+
+        foreach ($results as $key => $data) {
+            echo '<ul>Data: ' . $key;
+            foreach ($data as $result){
+                echo '<li>' . $result['titolo'] . '<br>' . $result['autore'] . '<br>' . $result['testo'] . '</li>';
+            }
+            echo '</ul>';
+    }   
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snacks</title>
+</head>
+<body>
+    <h1>Fatto!</h1>
+</body>
+</html>
